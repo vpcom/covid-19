c68aec0dca4243750dc68c830d0270402bbcd896
Vincent Perrin <vpcom01@gmail.com>
2020-04-13 15:14:23 +0200

    feat(text): description of data and cleanup

--- src/app/cases-deaths/cases-deaths.component.ts
+++ src/app/cases-deaths/cases-deaths.component.ts
@@ -1,19 +1,3 @@
-// import { Component, OnInit } from '@angular/core';
-
-// @Component({
-//   selector: 'app-cases-deaths',
-//   templateUrl: './cases-deaths.component.html',
-//   styleUrls: ['./cases-deaths.component.scss']
-// })
-// export class CasesDeathsComponent implements OnInit {
-
-//   constructor() { }
-
-//   ngOnInit(): void {
-//   }
-
-// }
-
 
 import {Component, OnInit, ViewChild} from '@angular/core';
 import {MatSort} from '@angular/material/sort';
@@ -30,23 +14,8 @@ export class CasesDeathsComponent implements OnInit {
 
   constructor(private covidService: CovidService) { }
 
-
-  // ELEMENT_DATA: countryStat[] = [
-  //   { countryName: 'Hydrogen', population: 1.0079, deaths: 111},
-  //   { countryName: 'Helium', population: 4.0026, deaths: 22},
-  //   { countryName: 'Lithium', population: 6.941, deaths: 333},
-  //   { countryName: 'Beryllium', population: 9.0122, deaths: 333},
-  //   { countryName: 'Boron', population: 10.811, deaths: 333},
-  //   { countryName: 'Carbon', population: 12.0107, deaths: 333},
-  //   { countryName: 'Nitrogen', population: 14.0067, deaths: 333},
-  //   { countryName: 'Oxygen', population: 15.9994, deaths: 333},
-  //   { countryName: 'Fluorine', population: 18.9984, deaths: 333},
-  //   { countryName: 'Neon', population: 20.1797, deaths: 333},
-  // ];
-
-
   displayedColumns: string[] = ['name', 'population', 'deaths', 'deathsRatio'];
-  // dataSource = new MatTableDataSource(this.ELEMENT_DATA);
+
   dataSource;
   populations$: Observable<any>;
   deaths$: Observable<any>;
@@ -55,8 +24,6 @@ export class CasesDeathsComponent implements OnInit {
   @ViewChild(MatSort, {static: true}) sort: MatSort;
 
   ngOnInit() {
-
-
 
     this.populations$ = this.covidService.getPopulations();
     this.deaths$ = this.covidService.getDeaths();
